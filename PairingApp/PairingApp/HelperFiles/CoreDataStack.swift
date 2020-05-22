e5e5fd1d04884999e585ad01de5786e7a78425a0
Beth <morris.bethany66@gmail.com>
2020-05-22 09:26:01 -0600

    starting

---
+++ PairingApp/PairingApp/HelperFiles/CoreDataStack.swift
@@ -0,0 +1,27 @@
+//
+//  CoreDataStack.swift
+//  PairingApp
+//
+//  Created by Bethany Morris on 5/22/20.
+//  Copyright © 2020 Bethany M. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "TaskApp")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    static var context: NSManagedObjectContext { return container.viewContext }
+    
+} //End
---
+++ PairingApp/PairingApp/Models/Person.swift
@@ -0,0 +1,12 @@
+//
+//  Person.swift
+//  PairingApp
+//
+//  Created by Bethany Morris on 5/22/20.
+//  Copyright © 2020 Bethany M. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
